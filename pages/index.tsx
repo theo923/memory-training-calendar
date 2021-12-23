58db64942eee7df08253f1d6b2d43ceee707dbb2
theo923 <theotam912@gmail.com>
2021-12-23 16:09:58 +0800

    use GraphQL & Apollo instead of RestAPI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps } from "next";
 import Head from "next/head";
 import React, { useState } from "react";
 import Layout from "../components/Layout";
@@ -8,15 +8,24 @@ import TaskBoard from "../components/Tasks";
 import Board, { UserTaskProps } from "../components/Board";
 import JobCreationBoard, { initialCard } from "../components/JobCreationBoard";
 import ModifyBoard from "../components/ModifyBoard";
-import axios from "axios";
-import { DEFAULT_KEY, NEXT_PUBLIC_API_URL, RETREIVE_STRING } from "../lib/env";
+import { client, DEFAULT_HEADERS } from "../lib/apollo";
+// import { useCookies } from "react-cookie";
+import { CALENDAR_QUERY } from "../lib/queries/graphql-calendar";
 
 const App = ({ tasks = {} }): JSX.Element => {
   const [target, setTarget] = useState(new Date());
   const [userTasks, setUserTasks] = useState(tasks || {})
   const [targetedTask, setTargetedTask] = useState<UserTaskProps>(initialCard)
-  console.log(tasks)
-  console.log(userTasks)
+  // const [cookies, setCookie, removeCookie] = useCookies(['calendar-user-token']);
+  // setCookie('calendar-user-token', '', {
+  //   path: "/",
+  //   maxAge: 3600,
+  //   sameSite: true,
+  // })
+
+  // console.log(tasks)
+  // console.log(userTasks)
+
 
   return (
     <>
@@ -59,26 +68,45 @@ const App = ({ tasks = {} }): JSX.Element => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const config = {
-    headers: {
-      "Authorization": `Bearer ${DEFAULT_KEY}`
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  try {
+    if (!req.cookies['calendar-user-token'])
+      return {
+        props: {
+          tasks: {}
+        }
+      }
+
+    const { data: { tasks: { data } } } =
+      await client.query({
+        query: CALENDAR_QUERY,
+        context: DEFAULT_HEADERS(req.cookies['calendar-user-token'])
+      })
+
+    const tasks = data.filter((each) => each?.attributes!['targetedDate'].length > 0)
+
+    const returnVal = {}
+    tasks.forEach((task) => {
+      task.attributes.targetedDate.forEach((date) => {
+        const { t_date } = date
+        const returnObject = { id: parseInt(task.id), ...task?.attributes }
+        if (returnVal![t_date])
+          returnVal[t_date].push(returnObject)
+        else returnVal[t_date] = [returnObject]
+      })
+    })
+
+    return {
+      props: {
+        tasks: returnVal
+      }
     }
-  }
-
-  const tasksData = await axios.get(`${NEXT_PUBLIC_API_URL}/${RETREIVE_STRING}`, config)
-  const tasks = tasksData.data
-
-  const returnVal = {}
-  tasks.forEach((task) => {
-    if (task?.attributes && returnVal[task?.attributes!["targetedDate"]])
-      returnVal[task?.attributes!["targetedDate"]].push(task?.attributes)
-    else returnVal[task?.attributes!["targetedDate"]] = [task?.attributes]
-  })
-
-  return {
-    props: {
-      tasks: returnVal
+  } catch (err) {
+    console.log(err)
+    return {
+      props: {
+        tasks: {}
+      }
     }
   }
 }
