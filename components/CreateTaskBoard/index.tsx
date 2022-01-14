fb69ff661aa95aa00ce52f48df8542e159766a81
theo923 <theotam912@gmail.com>
2022-01-14 09:00:09 +0800

    set loading identifier & minimized the code

--- components/CreateTaskBoard/index.tsx
+++ components/CreateTaskBoard/index.tsx
@@ -1,25 +1,10 @@
-import React, { ChangeEvent, useContext, useEffect, useState } from 'react'
-import { TaskColorProps, TaskProps, UserProps, UserTasksProps } from 'lib/interface'
-import { controlTaskTitle, controlTaskDescription } from 'lib/controller/controlTask'
-import { initializeUserTask, initializeTask } from 'lib/initialize'
-import { getFullDate } from 'lib/get/getDate'
-import styled from 'styled-components'
+import React, { useContext, useEffect} from 'react'
+import { TaskColorProps, UserProps, UserTasksProps } from 'lib/interface'
 import Box from 'styled/Box'
 import Button from 'styled/Button'
-import Flex from 'styled/Flex'
-import Input from 'styled/Input'
-import Text from 'styled/Text'
-import default_schedule from 'lib/utils/default_schedule'
-import axios from 'axios'
-import { refreshData } from 'lib/utils/refresh_data'
-import ColorPanel from 'components/ServerSettings/ColorPalette'
 import { ModalContext } from 'components/Modal/ModalContext'
 import CreateTaskBoardExtend from './extend'
-import SlateTextBox from 'styled/SlateTextBox'
-
-const InputText = styled(Box)`
-  align-self: center;
-`
+import CreateTaskBoardDefaultLayout from './defaultLayout'
 
 interface Props {
   userTasks: UserTasksProps,
@@ -37,35 +22,6 @@ const CreateTaskBoard: React.FC<Props> = ({
   colorPalette,
 }): JSX.Element => {
   const modalContext = useContext(ModalContext)
-  const [inputVal, setInputVal] = useState<TaskProps>(initializeTask)
-  const [status, setStatus] = useState<string>('')
-
-  useEffect(() => {
-    if (!userTasks![getFullDate(target)])
-      initializeUserTask(setUserTasks, target)
-  }, [target])
-
-  const handleSubmit = async () => {
-    try {
-      await axios.post('/api/createTask', {
-        userID: currentUser.id,
-        userName: currentUser.username,
-        taskTitle: inputVal.taskTitle,
-        taskDescription: inputVal.taskDescription,
-        targetedDate: default_schedule(target),
-        taskColor: inputVal.taskColor
-      }).then(({ data: { success } }) => {
-        if (success)
-          refreshData()
-        else
-          setStatus('Failed to add task, please try again...')
-      })
-    }
-    catch (err) {
-      console.log('Failed to add task...')
-      setStatus('Failed to add task, please try again...')
-    }
-  }
 
   useEffect(() => {
     if (modalContext.modalIsOpen) {
@@ -89,57 +45,13 @@ const CreateTaskBoard: React.FC<Props> = ({
   return (
     <Box data-test="component-createTaskBoard">
       <Button onClick={() => modalContext.setModalIsOpen(true)}>Extend</Button>
-      <>
-        <InputText
-          fontSize={['20px', null, '20px']}
-          lineHeight={['20px', null, '28px']}
-        >
-          Title:
-        </InputText>
-        <Input
-          value={inputVal.taskTitle}
-          onChange={(e: ChangeEvent<HTMLInputElement>) => controlTaskTitle(setInputVal, e)}
-        />
-        <InputText
-          fontSize={['20px', null, '20px']}
-          lineHeight={['20px', null, '28px']}
-        >
-          Description:
-        </InputText>
-        <SlateTextBox callChangeFunction={controlTaskDescription} insideObject changeHook={setInputVal} />
-        <InputText
-          fontSize={['20px', null, '20px']}
-          lineHeight={['20px', null, '28px']}
-          mr='2'
-        >
-          Static Colors:
-        </InputText>
-        <ColorPanel
-          currentValue={inputVal?.taskColor}
-          setInputVal={setInputVal}
-          colors={colorPalette?.color_static}
-          inputProperties='taskColor'
-        />
-        <InputText
-          fontSize={['20px', null, '20px']}
-          lineHeight={['20px', null, '28px']}
-          mr='2'
-        >
-          Gradient Colors:
-        </InputText>
-        <ColorPanel
-          currentValue={inputVal?.taskColor}
-          setInputVal={setInputVal}
-          colors={colorPalette?.color_gradient}
-          inputProperties='taskColor'
-        />
-        <Box />
-        <Flex justifyContent='space-around'>
-          <Button onClick={() => handleSubmit()}>Submit</Button>
-          <Button onClick={() => setInputVal(initializeTask)}>Reset</Button>
-        </Flex>
-      </>
-      {status.length > 0 && <Text color='red'>{status}</Text>}
+      <CreateTaskBoardDefaultLayout
+        currentUser={currentUser}
+        userTasks={userTasks}
+        setUserTasks={setUserTasks}
+        target={target}
+        colorPalette={colorPalette}
+      />
     </Box>
   )
 }
