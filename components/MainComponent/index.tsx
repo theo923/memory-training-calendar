724eb1d48f176716b8d191a1f58bd3f4643fc43b
theo923 <theotam912@gmail.com>
2022-04-10 14:51:24 +0100

    fixed background problem

--- components/MainComponent/index.tsx
+++ components/MainComponent/index.tsx
@@ -1,24 +1,38 @@
+import { UserContext } from 'components/User';
 import { childNode } from 'lib/interface';
-import React from 'react'
+import React, { useContext } from 'react'
+import styled, { css } from 'styled-components';
 import Box from 'styled/Box';
 
 interface Props {
   children?: childNode;
 }
 
+const MainComponentLayout = styled(Box) <{ bgcolor: string }>`
+  height: 100%; 
+
+  ${({ bgcolor }) => css`
+    background: ${bgcolor}
+  `}
+`
+
 const MainComponent: React.FC<Props> = ({
   children
 }) => {
+  const userInfo = useContext(UserContext)
+  
   return (
-    <Box
+    <MainComponentLayout
       data-test='component-mainComponent'
+      position='relative'
+      bgcolor={userInfo?.userSettings?.bgColor || 'linear-gradient(120deg, #d4fc79 0%, #96e6a1 100%);'}
       minHeight={['auto', 'auto', 'auto', '1119px']}
       minWidth={["20px", '573px', "780px", "921px"]}
       px={['0', '20px']}
       mx={['0', null, 'auto']}
     >
       {children}
-    </Box>
+    </MainComponentLayout>
   )
 }
 
